4580f2aa653f211e82e7eaff3079108410f8f5e6
vicecsilva@gmail.com <victor.candido@nb-vcandido.grupoiv2.local>
2020-04-08 08:54:58 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ app.post('/', (req, res) => {
     const name = time + bites;
     const { finalName = 'teste', templateResumo, templateOrcamentaria } = req.body;
 
+    console.log(templateResumo)
+    console.log(templateOrcamentaria)
+
     const configResumo = { 
         format: 'A4', 
         border: '10px' 
