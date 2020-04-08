e36a287b3d730e4aea4288a9a6f6bc2ca672d5f7
vicecsilva@gmail.com <victor.candido@nb-vcandido.grupoiv2.local>
2020-04-07 21:18:47 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,85 @@
+const merge = require('easy-pdf-merge')
+const fs = require('fs')
+const pdf = require('html-pdf');
+const pdfParser = require('pdf-parse');
+const cors = require('cors')
+const express = require('express');
+const crypto = require('crypto')
+
+// const tmpl1 = fs.readFileSync(require.resolve('./test/template.html'), 'utf8')
+// const tmpl2 = fs.readFileSync(require.resolve('./test/template3.html'), 'utf8')
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.post('/', (req, res) => {
+    const time = new Date().getTime();
+    const bites = crypto.randomBytes(4).toString('HEX');
+    const name = time + bites;
+    const { finalName = 'teste', templateResumo, templateOrcamentaria } = req.body;
+
+    const configResumo = { 
+        format: 'A4', 
+        border: '10px' 
+    }
+
+    let configOrc = { 
+        format: 'A4', 
+        orientation: 'landscape', 
+        border: '10px',
+        header: {
+            height: '15px',
+            contents: ``,
+        },
+        footer: {
+            height: "10px",
+            contents: ''
+        }
+    }
+
+    pdf.create(templateResumo, configResumo).toFile(__dirname + `/temp/${name}1.pdf`, async function (err, resResumo) {
+        if (err) {
+            return console.log(err)
+        } 
+        console.log('sucesso no primeiro', resResumo);
+
+        const { filename: fileResumo } = resResumo
+
+        const pdf1 = fs.readFileSync(require.resolve(fileResumo));
+        const data = await pdfParser(pdf1);
+    
+        configOrc.footer.paginationOffset = data.numpages;
+    
+        pdf.create(templateOrcamentaria, configOrc).toFile(__dirname + `/temp/${name}2.pdf`, function (err, resOrc) {
+            if (err) {
+                return console.log(err)
+            }       
+            console.log('sucesso no segundo', resOrc);
+
+            const { filename: fileOrc } = resOrc;
+    
+            const fileUrl = __dirname + `/temp/${name}final.pdf`;
+    
+            merge([fileResumo, fileOrc], fileUrl, err => {
+                if (err) {
+                    return console.log(err)
+                }   
+                console.log('Success Final')
+                res.download(fileUrl, `${finalName}.pdf`);
+                
+                setTimeout(() => {
+                    fs.unlink(fileResumo, () => console.log('ok'))
+                    fs.unlink(fileOrc, () => console.log('ok'))
+                    fs.unlink(fileUrl, () => console.log('ok'))
+                }, 2000);
+            })
+    
+        });
+    });
+})
+
+app.listen(3333)
